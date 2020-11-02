92f82c58f1ad2287c8f80b4c1045289fbc4cf071
Manuel Orantes Taboada <manuelorantes96@gmail.com>
2020-11-02 18:25:09 +0100

    Avanzo en clase profesor #17

--- Proyecto_EloPuertaElvira/src/Entity/Teacher.php
+++ Proyecto_EloPuertaElvira/src/Entity/Teacher.php
@@ -4,4 +4,38 @@ namespace App\Entity;
 
 class Teacher
 {
+    private $name;
+
+    /** @var Group[] */
+    private $groups = [];
+
+
+    public function __construct(string $name = 'Nuevo profesor', array $initialGroups = [])
+    {
+        $this->name = $name;
+
+        foreach ($initialGroups as $group){
+            $this->addGruop($group);
+        }
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name)
+    {
+        $this->name = $name;
+    }
+
+    public function getGroups(): array
+    {
+        return $this->groups;
+    }
+
+    public function addGroup(Group $group)
+    {
+        $this->groups[] = $group;
+    }
 }
