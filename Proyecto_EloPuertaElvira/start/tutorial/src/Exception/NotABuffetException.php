06709ec858d5c9f986ff1ff1f78b4b254b606a26
Manuel Orantes Taboada <manuelorantes96@gmail.com>
2020-11-02 14:48:06 +0100

    cambios

--- Proyecto_EloPuertaElvira/start/tutorial/src/Exception/NotABuffetException.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Exception;
-
-class NotABuffetException extends \Exception
-{
-    protected $message = 'Please do not mix the carnivorous and non-carnivorous dinosaurs. It will be a massacre!';
-}
