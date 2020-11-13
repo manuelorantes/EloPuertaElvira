a8d57e759784abbca4bd1fbd75a480b6d2974477
Manuel Orantes Taboada <manuelorantes96@gmail.com>
2020-11-13 22:57:13 +0100

    Añadida la clase que contendrá tantos a los jugadores del club como a los miembros del mismo #7 #13 #48

---
+++ src/Entity/Club.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace App\Entity;
+
+
+class Club
+{
+    private string $name;
+    private array $players = [];
+    private array $members = [];
+
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+    public function addPlayer(Player $player, bool $newMember = false) :void
+    {
+        $this->players[] = $player;
+
+        if ($newMember){
+            $this->members[] = $player;
+        }
+    }
+
+    public function playerBecomeMember(Player $player) :void
+    {
+        if(in_array($player, $this->players, true) && !in_array($player, $this->members, true)){
+            $this->members[] = $player;
+        }
+    }
+}
